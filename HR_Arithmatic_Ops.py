eb60d8464231f04f1eead833156950cdb1804503
deepammjoshi <59239428+deepammjoshi@users.noreply.github.com>
2020-09-17 14:26:14 -0700

    Update HR_Arithmatic_Ops.py

--- HR_Arithmatic_Ops.py
+++ HR_Arithmatic_Ops.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     a = int(input ('a:'))
     b = int(input ('b:'))
     print (a+b)
-    print (a-b)
+   # print (a-b)
     print (a*b)
     print (a//b)
     print (a/b)
